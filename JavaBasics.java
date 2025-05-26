5cef80eac6e01111d3f57fab6a53e5c00b301d90
rahul235004 <23rahul54@gmail.com>
2025-05-27 02:42:33 +0530

    java

---
+++ JavaBasics.java
@@ -0,0 +1,72 @@
+import java.util.*;
+public class JavaBasics {
+    public static void main(String args[]){
+        // System.out.print("hello world");
+        // System.out.print("hello world\nhello world");
+
+        // int a=10;
+        // int b =5;
+        // int sum = a+b;
+        // System.out.println(sum);
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Scanner sc= new Scanner(System.in) ;
+        // Boolean input = sc.nextBoolean();
+        // System.out.println(input);
+        
+
+        // sum of two numbers using Scanner
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int  sum =a+b;
+        // System.out.print(sum);
+
+       // product of two numbers using Scanner
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int  product =a*b;
+        // System.out.print(product);
+
+
+        // area of a circle using Scanner
+        // Scanner sc =new Scanner(System.in);
+        // float rad =sc.nextFloat();
+        // float area =3.14f*rad*rad;
+
+        // System.out.print(area);
+        // Scanner sc =new Scanner(System.in);
+        // char ch = 'Z';
+        // int num = ch;
+        // System.out.print(num); //  90
+        // int $ = 24;
+        // System.out.print($); //24
+        // System.out.println(2+2+"2"+2+2); //4222
+
+        // int left =5, right=10;
+        // int mid = left +  right/2;
+        // System.out.println(mid);;//10
+
+
+        // System.out.println(17/2);//8
+        // System.out.println(5-(10*15/2-(7+4))); //-59
+
+        // System.out.println((5>0 || (0==10/0))); // true
+
+        // System.out.println();
+
+
+        // swapping two numbers without using a third variable
+        // int a = 10, b= 7;
+        // a=a+b;
+        // b=a-b;
+        // a=a-b;
+        // System.out.println("a="+a);
+        // System.out.println("b="+b);
+
+        
+
+        }
+}
